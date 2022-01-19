3622dd1b4b296d09e0c31c9ee483fe081e0dcd6c
Florian Hauschild <florian.hauschild@fs.ei.tum.de>
2022-01-19 16:16:16 +0100

    updated update script and the current lightcontroll zip

--- update.sh
+++ update.sh
@@ -8,9 +8,16 @@ git pull origin main
 echo "update submodule contained inside script.lightcontroll"
 git submodule init
 git submodule update
+rm lightcontroller.py
+cp lightcontroller/python3/lightcontroller.py lightcontroller.py
+rm -rf lightcontroller
 cd ../..
 echo "Generate missing zips"
 python3 _repo_generator.py
+echo "reset script.lightcontroll manipulation for ziping"
+cd matrix/script.lightcontroll
+git stash
+cd ../..
 echo "update git and add new files"
 git add matrix/script.lightcontroll
 git add matrix/zips/*
